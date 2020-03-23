7ddf7efabde28ce354f1043919d3dc70e467f725
hewenwei038 <54757153+hewenwei038@users.noreply.github.com>
2020-03-23 22:28:29 +0800

    assi5

---
+++ assignment#5/static/js/download.js
@@ -0,0 +1,12 @@
+$.extend( $.fn.dataTable.FixedHeader, {
+    responsive: true
+} );
+
+$(document).ready(function() {
+    $('#myTable').DataTable( {
+        dom: 'Bfrtip',
+        buttons: [
+            'copy', 'csv', 'excel', 'pdf', 'print'
+        ]
+    } );
+} );
